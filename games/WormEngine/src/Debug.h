7cd641a151cf8bc9cfed3365b8d06d657f08c772
Sean Rodman <ser@amazon.com>
2018-04-23 10:11:46 -0400

    Make debug code more readable.

--- games/WormEngine/src/Debug.h
+++ games/WormEngine/src/Debug.h
@@ -1,6 +1,10 @@
 #include <GLFW/glfw3.h>
 
 #ifdef CALCULATE_FPS
+
+#define CALCULATE_FPS_IF_REQUIRED() \
+    calculate_and_display_fps()
+
 int num_frames = 0;
 double start_time = glfwGetTime();
 
@@ -16,12 +20,10 @@ void calculate_and_display_fps()
             num_frames=0;
         }
 }
-#endif
 
-#ifdef CALCULATE_FPS 
-#define CALCULATE_FPS_IF_REQUIRED() \
-    calculate_and_display_fps()
 #else
+
 #define CALCULATE_FPS_IF_REQUIRED()
+
 #endif
 
